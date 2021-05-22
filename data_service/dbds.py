d7ac084cc248493a8a210c822da639493b6beda3
benabbas <ben.abbas@mlytics.com>
2021-05-22 19:59:14 +0800

    add

---
+++ data_service/dbds.py
@@ -0,0 +1,12 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://192.168.32.128:27017/")
+
+mydb = myclient["mydatabase"]
+
+mycol = mydb["key"]
+
+def insert_db(message):
+    my_dict = {"message": message}
+    x = mycol.insert_one(my_dict)
+    return x.inserted_id
